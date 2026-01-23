b25f93e0aca9e652fb1e35996e0e0844a4ab5006
Nadil Bourkadi <nadil.bourkadi@believ.com>
2026-01-23 12:02:41 +0000

    Update treesitter for new nvim-treesitter API
    - Use new Lua API: require('nvim-treesitter').install() instead of :TSInstall
    - Auto-install common parsers on startup (lua, python, js, ts, etc.)
    - Requires tree-sitter-cli (installed via init.zsh)

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,9 +3,21 @@
 
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
   lazy = false,
   config = function()
+    local ts = require("nvim-treesitter")
+    ts.setup({})
+
+    -- Parsers to auto-install
+    local ensure_installed = {
+      "lua", "python", "javascript", "typescript", "tsx",
+      "json", "yaml", "toml", "bash", "html", "css",
+      "markdown", "markdown_inline", "vim", "vimdoc",
+    }
+
+    -- Install parsers asynchronously (new API - no-op if already installed)
+    ts.install(ensure_installed)
+
     -- Enable treesitter-based highlighting (Neovim 0.11+ native)
     vim.api.nvim_create_autocmd("FileType", {
       callback = function(args)
